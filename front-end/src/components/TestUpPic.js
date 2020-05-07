3708b01587fb444f0d858b69d1bed4582bc16351
nraenn <35445344+nraenn@users.noreply.github.com>
2020-05-07 18:38:24 +0700

    Upload multiple file
    Test upload multiple file to firebase.

---
+++ front-end/src/components/TestUpPic.js
@@ -0,0 +1,56 @@
+import React, { useState, Component } from 'react';
+import firebase, { storage } from '../firebase';
+
+const Upload = (props) => {
+
+    const [files, setFiles] = useState([])
+
+    const onFileChange = e => {
+        for (let i = 0; i < e.target.files.length; i++) {
+            const newFile = e.target.files[i];
+            newFile["id"] = Math.random();
+            // add an "id" property to each File object
+            setFiles(prevState => [...prevState, newFile]);
+        }
+    };
+
+    const onUploadSubmission = e => {
+        e.preventDefault(); // prevent page refreshing
+        const promises = [];
+        files.forEach(file => {
+            const uploadTask =
+                firebase.storage().ref().child(`image/${file.name}`).put(file);
+            promises.push(uploadTask);
+            uploadTask.on(
+                firebase.storage.TaskEvent.STATE_CHANGED,
+                snapshot => {
+                    const progress =
+                        ((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    if (snapshot.state === firebase.storage.TaskState.RUNNING) {
+                        console.log(`Progress: ${progress}%`);
+                    }
+                },
+                error => console.log(error.code),
+                async () => {
+                    const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+                    // do something with the url
+                }
+            );
+        });
+        Promise.all(promises)
+            .then(() => alert('All files uploaded'))
+            .catch(err => console.log(err.code));
+    }
+
+    return (
+        <form>
+            <label>Select Files
+            <input type="file" multiple onChange={onFileChange} />
+            </label>
+            <button onClick={onUploadSubmission}>Upload</button>
+        </form>
+    )
+
+}
+
+export default Upload;
