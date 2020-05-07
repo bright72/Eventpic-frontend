7c86159f0989a4eaa2db1f55cab2cd2766008104
nraenn <35445344+nraenn@users.noreply.github.com>
2020-05-07 18:39:33 +0700

    add path
    Add path for support test upload multiple file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,15 +6,17 @@ import Home from './Home.js'
 import Login from './Login.js'
 import Register from './Register.js'
 import ImageUpload from './components/ImageUpload.jsx';
+import TestUpload from './components/TestUpPic.js';
 import AddEvent from './AddEvent.js';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/Home" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/Login" component={Login} />
       <Route exact path="/Register" component={Register} />
-      <Route exact path="/Upload" component={ImageUpload} />
+      {/* <Route exact path="/Upload" component={ImageUpload} /> */}
+      <Route exact path="/TestUpPic" component={TestUpload} />
       <Route exact path="/AddEvent" component={AddEvent} />
     </Switch>
   </Router>
--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -12,7 +12,8 @@ class Home extends React.Component {
       <div className="App">
         <header className="App-header">
           <Link to="/Login"> <h1>Login</h1></Link>
-          <Link to="/Upload"> <h1>Upload</h1></Link>
+          {/* <Link to="/Upload"> <h1>Upload</h1></Link> */}
+          <Link to="/TestUpPic"> <h1>Upload</h1></Link>
           <Link to="/AddEvent"> <h1>Add Event</h1> </Link>
         </header >
       </div>
