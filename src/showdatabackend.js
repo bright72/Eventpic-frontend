a6eaa7f6612fa307c30073a645208d2c4bdd492b
harit suttisaksri <haritsuttisaksri@gmail.com>
2020-09-27 17:55:30 +0700

    Update showdatabackend.js

--- src/showdatabackend.js
+++ src/showdatabackend.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap'
 import './App.css';
 import axios from "axios";
 
@@ -8,37 +8,56 @@ class showdatabackend extends Component {
     constructor() {
         super()
         this.state = {
-            email: "",
+            email: "Null",
             img: []
         }
     }
 
-    componentDidMount = () => {
+
+    onclickrespond = () => {
+        console.log("teasdfsf");
         axios.get("/emailbakcend").then(responde => {
             console.log(responde.data);
             this.setState({
                 email: responde.data.email,
                 img: responde.data.img
-
             })
         })
     }
 
     render() {
-        const { img } = this.state
+        // const { img } = this.state
+        return (
+            <div>
+                <Button onClick={this.onclickrespond}>กดกูสิ</Button>
+                {/* <h1>email is : {this.state.email}</h1> */}
+                {
+                    (this.state.img.map((item) => {
+                        return (
+                            <div>
+                                <h1>{this.state.email}</h1>
+                                <h1>{item}</h1>
+                            </div>
+                        )
+                    }
+                    )
+                    )
 
-        return (this.state.img.map((item) => {
-            return (
-                <div>
-                    <h1>{this.state.email}</h1>
-                    <h1>{item}</h1>
-                </div>
-
-            )
-
-        }))
-    
+                }
+            </div>
+        )
     }
 }
 
 export default showdatabackend;
+
+//return (this.state.img.map((item) => {
+//     return (
+//         <div>
+//             <h1>{this.state.email}</h1>
+//             <h1>{item}</h1>
+//         </div>
+//     )
+// }
+// )
+//)
